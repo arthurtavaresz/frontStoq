4fe8ffc8dd7ca996446437492e7dfcc62bc60829
arthurtavaresz <arthurzimtavares1512@outlook.com>
2025-05-19 15:54:07 -0300

    modificações: novo botao na activity_main.xml; nova cor adicionada ao colors.xml; novo icone disponivel no drawlabe: ic_deleteVencido.png;

--- app/src/main/java/com/example/front/MainActivity.java
+++ app/src/main/java/com/example/front/MainActivity.java
@@ -39,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
         btnFreezer.setOnClickListener(v -> {
-            Log.d("DEBUG", "Intent do Freezer chamada!");
             startActivity(new Intent(this, FreezerActivity.class));
         });
 
